b740dbe28f4ce54461189651ce72075ba2d4b5d8
gfx31501362 <31501362@stu.zucc.edu.cn>
2018-07-12 08:48:22 +0800

    //初始版本，初始主界面

---
+++ app/src/main/java/chj31501362/zucc/com/rate/MainActivity.java
@@ -0,0 +1,15 @@
+package chj31501362.zucc.com.rate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: information");
+    }
+}
